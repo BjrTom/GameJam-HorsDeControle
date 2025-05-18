0c9489ada7b88edff5a4c94a156b489e076ffd44
JulesMEI <146710423+JulesMEI@users.noreply.github.com>
2025-05-18 21:13:16 +0200

    fix: missing Goal label, too short of a  cooldown between texts

--- gameplay-scene/Introduction/intro.gd
+++ gameplay-scene/Introduction/intro.gd
@@ -4,7 +4,7 @@ func _ready() -> void:
 	$IntroMusic.play()
 	$AnimationPlayer.play("appear")
 	$AnimationPlayer.play("text_fade")
-	await get_tree().create_timer(23).timeout
+	await get_tree().create_timer(25).timeout
 	$Button.visible = true
 	await get_tree().create_timer(0.5).timeout
 	$Button.disabled = false
@@ -15,8 +15,6 @@ func _on_button_pressed() -> void:
 	$Button.disabled = true
 	await get_tree().create_timer(0.5).timeout
 	$Button.visible = false
-	$AnimationPlayer.play("leave")
-	$AnimationPlayer.play("fade_audio")
-	await get_tree().create_timer(4.5).timeout
+	await get_tree().create_timer(2).timeout
 	$IntroMusic.stop()
 	get_tree().change_scene_to_file("res://gameplay-scene/game.tscn")
